51f91ba41eaa178bdc18465d11ef5101f5af2b98
eren-ay <eren@eren-Dizustu-Bilgisayar.local>
2024-03-14 12:06:53 +0300

    SubUser model created

---
+++ TVseries/lib/src/models/subUser.dart
@@ -0,0 +1,33 @@
+class SubUser {
+  String subId;
+
+  String name;
+
+  String surname;
+
+  String image;
+
+  String title;
+
+  String description;
+
+  int pin;
+
+  //List<Movie> Movies ;
+
+  //List<Movie> FavoriteFilms ;
+
+  //Movie LastWatched ;
+
+  //List<Censor> Censors ;
+
+  SubUser({
+    required this.subId,
+    required this.name,
+    required this.surname,
+    required this.image,
+    required this.title,
+    required this.description,
+    required this.pin,
+  }) {}
+}
