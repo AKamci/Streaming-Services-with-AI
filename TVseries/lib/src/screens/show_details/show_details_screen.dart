99aae1ff9ac949017d49cfa42f1c1dd136d303cd
eren-ay <erenay494@gmail.com>
2024-04-25 15:51:33 +0300

    censorship widget add

--- TVseries/lib/src/screens/show_details/show_details_screen.dart
+++ TVseries/lib/src/screens/show_details/show_details_screen.dart
@@ -1,12 +1,42 @@
+import 'package:external_video_player_launcher/external_video_player_launcher.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:tv_series/src/screens/show_details/widgets/censor_setting.dart';
 
+// ignore: must_be_immutable
 class ShowInfoPage extends StatelessWidget {
-  final String imageUrl = 'https://via.placeholder.com/350x150';
+  final String imageUrl =
+      'https://www.log.com.tr/wp-content/uploads/2024/02/deadpool-wolverine-filmi-icin-ilk-fragman-yayinlandi.jpg';
   final String title = 'Show Title';
   final String genre = 'Action, Drama';
   final String rating = '8.5/10';
   final String description =
       'This is a brief description of the show. It talks about the main plot, the characters, and why it is interesting.';
+  List<Map> mediaJSON = [
+    {
+      "description":
+          "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
+      "sources": [
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+      ],
+      "subtitle": "By Blender Foundation",
+      "thumb": "images/BigBuckBunny.jpg",
+      "title": "Big Buck Bunny"
+    },
+    {
+      "description":
+          "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
+      "sources": [
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+      ],
+      "subtitle": "By Blender Foundation",
+      "thumb": "images/BigBuckBunny.jpg",
+      "title": "Big Buck Bunny"
+    }
+  ];
+
+  ShowInfoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +47,12 @@ class ShowInfoPage extends StatelessWidget {
             expandedHeight: 200.0,
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
-              title: Text(title, style: TextStyle(fontSize: 16)),
-              background: Image.network(imageUrl, fit: BoxFit.cover),
+              background: Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
+            leading: Container(),
           ),
           SliverToBoxAdapter(
             child: Container(
@@ -27,6 +60,7 @@ class ShowInfoPage extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  Text(title, style: TextStyle(fontSize: 16)),
                   Row(
                     children: [
                       Expanded(
@@ -57,11 +91,42 @@ class ShowInfoPage extends StatelessWidget {
                             .colorScheme
                             .secondary, // foreground color
                       ),
-                      onPressed: () {
-                        // Add your action here
+                      onPressed: () async {
+                        var videoMap = mediaJSON[0];
+                        try {
+                          await ExternalVideoPlayerLauncher.launchVlcPlayer(
+                              videoMap["sources"][0],
+                              "video/mp4",
+                              {"title": videoMap["title"]});
+                        } catch (e) {
+                          print('Failed to launch external player: $e');
+                        }
                       },
                       child: Text('Watch Now'),
                     ),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Container(),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: Colors.deepOrange,
+                          ),
+                          height: 100,
+                          child: const checkBox(),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(),
+                      ),
+                    ],
                   ),
                 ],
               ),
